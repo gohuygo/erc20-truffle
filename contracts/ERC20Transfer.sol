b993cc41633ffabd180932f9cd2a021612463679
gohuygo <huy@gohuygo.com>
2018-11-08 11:10:49 -0800

    Remove 'god' file and only allow sending to non contract addresses

--- contracts/ERC20Transfer.sol
+++ contracts/ERC20Transfer.sol
@@ -9,11 +9,20 @@ library ERC20Transfer {
     mapping(address => uint256) balance;
   }
 
+  function isContract(address _addr) private view returns (bool) {
+    uint32 size;
+
+    assembly {
+      size := extcodesize(_addr)
+    }
+
+    return (size > 0);
+  }
+
   function transferTokens(UserBalance storage self, address _from, address _to, uint _amount) public returns(bool) {
     require(self.balance[_from] >= _amount);
+    require(!isContract(_to));
 
-    // self.balance[_from] -= _amount;
-    // self.balance[_to] += _amount;
     self.balance[_from] = self.balance[_from].subtract(_amount);
     self.balance[_to]   = self.balance[_to].add(_amount);
 
